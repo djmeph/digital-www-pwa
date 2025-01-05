2d5abd16cdfb9ececaaaa641cb870eef03f3b883
DJ Meph <info@djmeph.net>
2025-01-05 23:47:48 +0000

    import classes to link

--- libs/top-nav/component/src/lib/top-nav-component.tsx
+++ libs/top-nav/component/src/lib/top-nav-component.tsx
@@ -56,7 +56,16 @@ export function TopNavComponent() {
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 {navigation.map((item) => (
-                  <Link key={item.name} to={item.href} current={item.current}>
+                  <Link
+                    key={item.name}
+                    to={item.href}
+                    className={classNames(
+                      item.current
+                        ? 'bg-gray-900 text-white'
+                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'rounded-md px-3 py-2 text-sm font-medium'
+                    )}
+                  >
                     {item.name}
                   </Link>
                 ))}
